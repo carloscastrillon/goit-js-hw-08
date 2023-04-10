ade6ce0b0f813b6432912902128b249ed684f9f4
Carlos Andres Castrillon <caacastr@bancolombia.com.co>
2023-04-10 17:01:31 -0500

    Tarea 1 OK

---
+++ src/js/01-gallery.js
@@ -0,0 +1,25 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+// Descrito en la documentación
+import SimpleLightbox from "simplelightbox";
+// Importación adicional de estilos
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+console.log(galleryItems);
+const lstIimg = document.querySelector('.gallery');
+
+
+const li = galleryItems.map(img=>`
+<li class="gallery__item">
+   <a class="gallery__link" href=${img.original}>
+      <img class="gallery__image" src=${img.preview} alt=${img.description} />
+   </a>
+</li>
+`).join("");
+
+lstIimg.innerHTML=li;
+const imgScroll= new SimpleLightbox('.gallery a', { captionsData:'alt', captionDelay:250});
+imgScroll.on('show.simplelightbox');
+
