d7d4d882809fd4e235930b55fb592f4e207e3e85
Carlos Andres Castrillon <caacastr@bancolombia.com.co>
2023-04-10 20:44:14 -0500

    Tarea 2 OK- 3 falta Reload

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const feedbackForm = document.querySelector('.feedback-form');
+const emailTyped = document.querySelector('[name="email"]');
+const messageTyped = document.querySelector('[name="message"]')
+
+
+feedbackForm.addEventListener('input', throttle(typeData, 500));
+feedbackForm.addEventListener('submit', submitForm);
+
+function typeData() {
+    const email = emailTyped.value;
+    const message = messageTyped.value;
+    const formData = {
+        email,
+        message,
+    };
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+};
+
+function submitForm(event){
+    event.preventDefault();
+    const email = emailTyped.value;
+    const message = messageTyped.value;
+    const formData = {
+        email,
+        message,
+    };
+    console.log(formData);
+    feedbackForm.reset();
+    localStorage.clear();
+
+}
